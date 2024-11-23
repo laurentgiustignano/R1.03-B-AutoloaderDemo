89e076788b2c14130d5e0513c914a08226d4ce80
Laurent Giustignano <laurent.giustignano@free.fr>
2024-11-23 17:05:15 +0100

    Pas d'autoloading

---
+++ exemple.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'class/Mario.php';
+require_once 'class/Luigi.php';
+require_once 'class/Bowser.php';
+require_once 'class/Toad.php';
+
+
+$mario = new Mario();
+$luigi = new Luigi();
+$bowser = new Bowser();
+$toad = new Toad();
