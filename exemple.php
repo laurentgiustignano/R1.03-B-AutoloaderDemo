0a048c5dcc91b203560d0a63e6a1f550d7cfa212
Laurent Giustignano <laurent.giustignano@free.fr>
2024-11-23 17:12:16 +0100

    Ne fonctionne plus. Il faut enregistrer une autre arboresence

--- exemple.php
+++ exemple.php
@@ -1,12 +1,14 @@
 <?php
 
-require_once 'class/Mario.php';
-require_once 'class/Luigi.php';
-require_once 'class/Bowser.php';
-require_once 'class/Toad.php';
-
+spl_autoload_register(function($class) {
+  require_once "Nintendo/" . $class . ".php";
+});
 
 $mario = new Mario();
 $luigi = new Luigi();
 $bowser = new Bowser();
 $toad = new Toad();
+
+$batman = new Batman();
+$jocker = new Jocker();
+$robin = new Robin();
